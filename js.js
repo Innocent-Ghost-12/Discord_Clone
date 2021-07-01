05ee6cd63c2004f3c0ce3b8af5ace7b89d3c599c
Innocent-Ghost-12 <84078234+Innocent-Ghost-12@users.noreply.github.com>
2021-07-01 21:41:40 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+function go(){alert("Use a device with width greater than 768px or switch to DESKTOP MODE")};
+
+var btn = document.getElementById("toggle-button");
+btn.addEventListener("click", go);
+
+function options(){alert("No more languages available")};
+
+var btn = document.getElementById("language");
+btn.addEventListener("click", options);
+
+let numbers=[1,2,3,4,5]
+console.log(numbers)
